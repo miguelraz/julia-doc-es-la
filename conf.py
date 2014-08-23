92379e25f65cc64a115719f69370e18e0c8b70a0
Luis Benites Sánchez <lbenitesanchez@gmail.com>
2014-08-23 16:59:38 -0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -12,6 +12,7 @@
 # serve to show the default.
 
 import sys, os
+import juliadoc
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -25,7 +26,10 @@ sys.path.insert(0, os.path.abspath('sphinx'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax', 'julia', 'jlhelp']
+extensions = ['sphinx.ext.mathjax',
+              'juliadoc.julia',
+              'juliadoc.jldoctest',
+              'juliadoc.jlhelp']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
